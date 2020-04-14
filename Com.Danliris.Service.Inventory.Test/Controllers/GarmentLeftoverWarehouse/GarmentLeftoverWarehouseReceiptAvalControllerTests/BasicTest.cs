6236083e4e66279dcb06db17d8ff6fb41c568fe8
achensetyawati <achenzetya@gmail.com>
2020-04-14 08:52:15 +0700

    GarmentLeftoverWarehouseAval Test

---
+++ Com.Danliris.Service.Inventory.Test/Controllers/GarmentLeftoverWarehouse/GarmentLeftoverWarehouseReceiptAvalControllerTests/BasicTest.cs
@@ -0,0 +1,15 @@
+﻿using Com.Danliris.Service.Inventory.Lib.Models.GarmentLeftoverWarehouse.GarmentLeftoverWarehouseReceiptAvalModels;
+using Com.Danliris.Service.Inventory.Lib.Services.GarmentLeftoverWarehouse.GarmentLeftoverWarehouseReceiptAvalServices;
+using Com.Danliris.Service.Inventory.Lib.ViewModels.GarmentLeftoverWarehouse.GarmentLeftoverWarehouseReceiptAvalViewModels;
+using Com.Danliris.Service.Inventory.Test.Helpers;
+using Com.Danliris.Service.Inventory.WebApi.Controllers.v1.GarmentLeftoverWarehouse.GarmentLeftoverWarehouseReceiptAvalControllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Inventory.Test.Controllers.GarmentLeftoverWarehouse.GarmentLeftoverWarehouseReceiptAvalControllerTests
+{
+    public class BasicTest : BaseControllerTest<GarmentLeftoverWarehouseReceiptAvalController, GarmentLeftoverWarehouseReceiptAval, GarmentLeftoverWarehouseReceiptAvalViewModel, IGarmentLeftoverWarehouseReceiptAvalService>
+    {
+    }
+}
