6382ac5c6668ad63d89d51b7743e235e2ef9b023
achensetyawati <achenzetya@gmail.com>
2020-05-14 09:43:37 +0700

    migration Expenditure FinishedGoods

--- Com.Danliris.Service.Inventory.Lib/Migrations/20200512032045_initial_ExpenditureFinishedGoods.Designer.cs
+++ Com.Danliris.Service.Inventory.Lib/Migrations/20200512032045_initial_ExpenditureFinishedGoods.Designer.cs
@@ -4,14 +4,16 @@ using Com.Danliris.Service.Inventory.Lib;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Com.Danliris.Service.Inventory.Lib.Migrations
 {
     [DbContext(typeof(InventoryDbContext))]
-    partial class InventoryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200512032045_initial_ExpenditureFinishedGoods")]
+    partial class initial_ExpenditureFinishedGoods
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
