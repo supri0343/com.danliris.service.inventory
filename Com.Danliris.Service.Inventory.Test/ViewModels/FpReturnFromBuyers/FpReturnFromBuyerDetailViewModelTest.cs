bcc17f177651e055c872a3af19952bdd2d6e1d4b
adepamungkas <ade.pamungkas9@gmail.com>
2020-08-11 15:37:18 +0700

    Update FpReturnFromBuyerDetailViewModelTest.cs

--- Com.Danliris.Service.Inventory.Test/ViewModels/FpReturnFromBuyers/FpReturnFromBuyerDetailViewModelTest.cs
+++ Com.Danliris.Service.Inventory.Test/ViewModels/FpReturnFromBuyers/FpReturnFromBuyerDetailViewModelTest.cs
@@ -9,7 +9,7 @@ namespace Com.Danliris.Service.Inventory.Test.ViewModels.FpReturnFromBuyers
     public class FpReturnFromBuyerDetailViewModelTest
     {
         [Fact]
-        public void should_succes_instantiate()
+        public void should_succes_instantiate_OrderTypeIntegrationViewModel()
         {
             OrderTypeIntegrationViewModel viewModel = new OrderTypeIntegrationViewModel()
             {
@@ -22,6 +22,26 @@ namespace Com.Danliris.Service.Inventory.Test.ViewModels.FpReturnFromBuyers
             Assert.Equal("Code", viewModel.Code);
             Assert.Equal(1, viewModel.Id);
 
+
         }
+
+        [Fact]
+        public void should_succes_instantiate()
+        {
+
+            ProductionOrderIntegrationViewModel viewModel = new ProductionOrderIntegrationViewModel()
+            {
+                OrderQuantity = 1,
+                IsCompleted = true,
+                DistributedQuantity = 1,
+                OrderType = new OrderTypeIntegrationViewModel()
+            };
+            Assert.Equal(1, viewModel.OrderQuantity);
+            Assert.True(viewModel.IsCompleted);
+            Assert.Equal(1, viewModel.DistributedQuantity);
+            Assert.NotNull(viewModel.OrderType);
+        }
+
+
     }
 }
