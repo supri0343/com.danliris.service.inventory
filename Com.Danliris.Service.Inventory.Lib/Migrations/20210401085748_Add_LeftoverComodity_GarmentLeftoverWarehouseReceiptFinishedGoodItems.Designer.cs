ac60b846ff3b71d89c2ab66406a2998caef57815
achensetyawati <setyawati@moonlay.com>
2021-04-01 17:13:09 +0700

    update garmentleftoverWarehouse FinishedGood

--- Com.Danliris.Service.Inventory.Lib/Migrations/20210401085748_Add_LeftoverComodity_GarmentLeftoverWarehouseReceiptFinishedGoodItems.Designer.cs
+++ Com.Danliris.Service.Inventory.Lib/Migrations/20210401085748_Add_LeftoverComodity_GarmentLeftoverWarehouseReceiptFinishedGoodItems.Designer.cs
@@ -4,14 +4,16 @@ using Com.Danliris.Service.Inventory.Lib;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Com.Danliris.Service.Inventory.Lib.Migrations
 {
     [DbContext(typeof(InventoryDbContext))]
-    partial class InventoryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210401085748_Add_LeftoverComodity_GarmentLeftoverWarehouseReceiptFinishedGoodItems")]
+    partial class Add_LeftoverComodity_GarmentLeftoverWarehouseReceiptFinishedGoodItems
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
