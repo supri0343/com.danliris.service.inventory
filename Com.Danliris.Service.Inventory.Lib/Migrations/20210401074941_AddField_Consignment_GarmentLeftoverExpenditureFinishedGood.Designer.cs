035c765041d0a11eeebb1866b3084acfca516bf1
Helmi Adi Prasetyo <helmi.prasetyo12@gmail.com>
2021-04-01 14:56:24 +0700

    Add Field Consignment GarmentLeftoverExpenditureFinishedGood

--- Com.Danliris.Service.Inventory.Lib/Migrations/20210401074941_AddField_Consignment_GarmentLeftoverExpenditureFinishedGood.Designer.cs
+++ Com.Danliris.Service.Inventory.Lib/Migrations/20210401074941_AddField_Consignment_GarmentLeftoverExpenditureFinishedGood.Designer.cs
@@ -4,14 +4,16 @@ using Com.Danliris.Service.Inventory.Lib;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Com.Danliris.Service.Inventory.Lib.Migrations
 {
     [DbContext(typeof(InventoryDbContext))]
-    partial class InventoryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210401074941_AddField_Consignment_GarmentLeftoverExpenditureFinishedGood")]
+    partial class AddField_Consignment_GarmentLeftoverExpenditureFinishedGood
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
