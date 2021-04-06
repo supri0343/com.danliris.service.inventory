9011985b9eba2acd984d7b9cd581e75172d0b8ff
achensetyawati <setyawati@moonlay.com>
2021-04-06 14:23:28 +0700

    update garmentLeftoverWarehouse Stock add leftover comodity

--- Com.Danliris.Service.Inventory.Lib/Migrations/20210406064449_add_LeftoverComodity_LeftoverWarehouseStocks.Designer.cs
+++ Com.Danliris.Service.Inventory.Lib/Migrations/20210406064449_add_LeftoverComodity_LeftoverWarehouseStocks.Designer.cs
@@ -4,14 +4,16 @@ using Com.Danliris.Service.Inventory.Lib;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Com.Danliris.Service.Inventory.Lib.Migrations
 {
     [DbContext(typeof(InventoryDbContext))]
-    partial class InventoryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210406064449_add_LeftoverComodity_LeftoverWarehouseStocks")]
+    partial class add_LeftoverComodity_LeftoverWarehouseStocks
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
