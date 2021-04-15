c352f8962d9199642a166449234fc516232ee46a
Helmi Adi Prasetyo <helmi.prasetyo12@gmail.com>
2021-04-15 15:16:04 +0700

    Initial Service GarmentLeftoverWarehouseReportExpenditure

--- Com.Danliris.Service.Inventory.Lib/Helpers/APIEndpoint.cs
+++ Com.Danliris.Service.Inventory.Lib/Helpers/APIEndpoint.cs
@@ -12,6 +12,7 @@ namespace Com.Danliris.Service.Inventory.Lib.Helpers
         public static string Purchasing { get; set; }
         public static string Sales { get; set; }
         public static string GarmentProduction { get; set; }
+        public static string PackingInventory { get; set; }
         
     }
 }
