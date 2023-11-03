9f6776061c97b493dc867c689890622e29b11b1f
supri0343 <63761987+supri0343@users.noreply.github.com>
2023-11-03 11:06:53 +0700

    Enahnce-Add-Report-MonitoringLogHistoryDMS

---
+++ Com.Danliris.Service.Inventory.WebApi/Controllers/v1/LogHistoryController.cs
@@ -0,0 +1,97 @@
+﻿using Com.Danliris.Service.Inventory.Lib.Services;
+using Com.Danliris.Service.Inventory.Lib.Services.LogHistories;
+using Com.Danliris.Service.Inventory.WebApi.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Com.Danliris.Service.Inventory.WebApi.Controllers.v1
+{
+    [Produces("application/json")]
+    [ApiVersion("1.0")]
+    [Route("v{version:apiVersion}/log-history")]
+    [Authorize]
+    public class LogHistoryController : Controller
+    {
+        protected IIdentityService IdentityService;
+        protected readonly IValidateService ValidateService;
+        protected readonly ILogHistoryService Service;
+        protected readonly string ApiVersion;
+
+        public LogHistoryController(IIdentityService identityService, IValidateService validateService, ILogHistoryService service)
+        {
+            IdentityService = identityService;
+            ValidateService = validateService;
+            Service = service;
+            ApiVersion = "1.0.0";
+        }
+
+        protected void VerifyUser()
+        {
+            IdentityService.Username = User.Claims.ToArray().SingleOrDefault(p => p.Type.Equals("username")).Value;
+            IdentityService.Token = Request.Headers["Authorization"].FirstOrDefault().Replace("Bearer ", "");
+            IdentityService.TimezoneOffset = Convert.ToInt32(Request.Headers["x-timezone-offset"]);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetReportAll(DateTime dateFrom, DateTime dateTo, int page, int size, string Order = "{}")
+        {
+            VerifyUser();
+            try
+            {
+                int offset = Convert.ToInt32(Request.Headers["x-timezone-offset"]);
+                string accept = Request.Headers["Accept"];
+                if (page == 0)
+                {
+                    page = 1;
+                    size = 25;
+                }
+                var data = await Service.GetMonitoring(dateFrom, dateTo, page, size);
+
+                return Ok(new
+                {
+                    apiVersion = ApiVersion,
+                    data = data.Item1,
+                    info = new { total = data.Item2 },
+                    message = General.OK_MESSAGE,
+                    statusCode = General.OK_STATUS_CODE
+                });
+            }
+            catch (Exception e)
+            {
+                Dictionary<string, object> Result =
+                    new ResultFormatter(ApiVersion, General.INTERNAL_ERROR_STATUS_CODE, e.Message)
+                    .Fail();
+                return StatusCode(General.INTERNAL_ERROR_STATUS_CODE, Result);
+            }
+        }
+
+        [HttpGet("download")]
+        public async Task<IActionResult> GetExcelLogHistories(DateTime dateFrom, DateTime dateTo)
+        {
+            VerifyUser();
+            try
+            {
+                byte[] xlsInBytes;
+                var xls = await Service.GenerateExcelReport(dateFrom, dateTo);
+
+                string filename = String.Format("Laporan Aktivitas (Log) User - {0} - {1}.xlsx", dateFrom.ToString("dd-MM-yyyy"), dateTo.ToString("dd-MM-yyyy"));
+
+                xlsInBytes = xls.ToArray();
+                var file = File(xlsInBytes, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", filename);
+                return file;
+            }
+            catch (Exception e)
+            {
+                Dictionary<string, object> Result =
+                    new ResultFormatter(ApiVersion, General.INTERNAL_ERROR_STATUS_CODE, e.Message)
+                    .Fail();
+                return StatusCode(General.INTERNAL_ERROR_STATUS_CODE, Result);
+            }
+        }
+        
+    }
+}
