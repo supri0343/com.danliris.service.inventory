056ec4b37cae0037b51c331fb5a27f933d21658c
supri0343 <63761987+supri0343@users.noreply.github.com>
2023-11-03 11:06:53 +0700

    Enahnce-Add-Report-MonitoringLogHistoryDMS

--- Com.Danliris.Service.Inventory.Lib/Services/LogHistory/ILogHistoryService.cs
+++ Com.Danliris.Service.Inventory.Lib/Services/LogHistory/ILogHistoryService.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Com.Danliris.Service.Inventory.Lib.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Com.Danliris.Service.Inventory.Lib.Services.LogHistories
 {
     public interface ILogHistoryService
     {
         void CreateAsync(string division, string activity);
+        Task<Tuple<List<LogHistoryViewModel>, int>> GetMonitoring(DateTime dateFrom, DateTime dateTo, int page, int size);
+        Task<MemoryStream> GenerateExcelReport(DateTime dateFrom, DateTime dateTo);
     }
 }
