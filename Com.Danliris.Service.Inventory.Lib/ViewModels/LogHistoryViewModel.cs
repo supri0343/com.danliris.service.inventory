471ea333c8cc309c24f79cc888b6ba5084bea9fc
supri0343 <63761987+supri0343@users.noreply.github.com>
2023-11-03 11:06:53 +0700

    Enahnce-Add-Report-MonitoringLogHistoryDMS

---
+++ Com.Danliris.Service.Inventory.Lib/ViewModels/LogHistoryViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Inventory.Lib.ViewModels
+{
+    public class LogHistoryViewModel
+    {
+        public string Division { get; set; }
+        public string Activity { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+    }
+
+    public class LogHistoryViewModelToUI
+    {
+        public string Division { get; set; }
+        public string Activity { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
