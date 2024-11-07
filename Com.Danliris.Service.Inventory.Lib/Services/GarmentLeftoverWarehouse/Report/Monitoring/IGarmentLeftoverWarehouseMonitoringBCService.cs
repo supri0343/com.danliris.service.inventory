02b086f200db8a622b9ccb170e9c6a8906fd8ab3
supri0343 <63761987+supri0343@users.noreply.github.com>
2024-11-07 14:11:00 +0700

    2024/23/Enhance/Add-LeftOver-BCFeature

---
+++ Com.Danliris.Service.Inventory.Lib/Services/GarmentLeftoverWarehouse/Report/Monitoring/IGarmentLeftoverWarehouseMonitoringBCService.cs
@@ -0,0 +1,12 @@
+﻿using Com.Danliris.Service.Inventory.Lib.ViewModels.GarmentLeftoverWarehouse.Monitoring;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Inventory.Lib.Services.GarmentLeftoverWarehouse.Report.Monitoring
+{
+    public interface IGarmentLeftoverWarehouseMonitoringBCService
+    {
+        List<GarmentLeftoverWarehouseMonitoringViewModel> GetReportQuery(List<GarmentLeftoverWarehouseMonitoringParameterViewModel> param);
+    }
+}
---
+++ Com.Danliris.Service.Inventory.Lib/ViewModels/GarmentLeftoverWarehouse/Monitoring/GarmentLeftoverWarehouseMonitoringParameterViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Inventory.Lib.ViewModels.GarmentLeftoverWarehouse.Monitoring
+{
+    public class GarmentLeftoverWarehouseMonitoringParameterViewModel
+    {
+        public string UENNo { get; set; }
+        public string PO { get; set; }
+    }
+}
